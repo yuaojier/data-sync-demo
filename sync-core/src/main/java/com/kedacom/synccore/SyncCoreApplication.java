7f2e0ff75386c8d036707520307622dd54af3064
yuao <sunyuao@kedacom.com>
2020-02-13 13:47:06 +0800

    dao

---
+++ sync-core/src/main/java/com/kedacom/synccore/SyncCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kedacom.synccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncCoreApplication.class, args);
+    }
+
+}
---
+++ sync-core/src/test/java/com/kedacom/synccore/SyncCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedacom.synccore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
